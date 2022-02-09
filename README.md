09fb4c741ada9506313bf07e1e0aa69059dd7fe6
Timing <time_crossing@163.com>
2022-02-09 17:14:31 +0800

    1001题提交

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LeetCode-Solution
+# LeetCode-p1001.Solution
 LeetCode题目 多种语言的AC代码仓库
--- java/p1001/Solution.java
+++ java/p1001/Solution.java
@@ -1,3 +1,5 @@
+package p1001;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
