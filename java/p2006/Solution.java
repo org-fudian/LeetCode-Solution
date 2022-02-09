097b909de572d0053fb73dd6d81a6d65432548e2
Timing <time_crossing@163.com>
2022-02-09 17:18:33 +0800

    2006提交

---
+++ java/p2006/Solution.java
@@ -0,0 +1,26 @@
+package p2006;
+
+/**
+ * 1 ms time:85.00%
+ * 41M memory:6.00%
+ * @author Timing
+ * @since 2022/2/9 5:17 PM
+ */
+class Solution {
+	public int countKDifference(int[] nums, int k) {
+		int[] count = new int[201];
+		int ans = 0;
+		for (int i = 0; i < nums.length; i++){
+			int a = nums[i] - k;
+			int b = nums[i] + k;
+			if (a >= 0 && a < 200 && count[a] > 0){
+				ans+=count[a];
+			}
+			if (b >= 0 && b < 200 && count[b] > 0){
+				ans+=count[b];
+			}
+			count[nums[i]]++;
+		}
+		return ans;
+	}
+}
