996db6907b91ccffba97ece90b202fb691e14062
Timing <time_crossing@163.com>
2022-02-11 13:54:47 +0800

    p165 p1984

--- java/p1984/Solution.java
+++ java/p1984/Solution.java
@@ -3,13 +3,14 @@ package p1984;
 import java.util.Arrays;
 
 /**
- * 5ms time:14.48%
- * 41.3M memory:13.79%
+ * 4ms time:100%
+ * 41.3M memory:8.28%
  * @author Timing
  * @since 2022/2/11 10:36 AM
  */
 /*
    k的最小差值，易得当有序时，滑动窗口长度为k的左端点数字和右端点数字的最小差为解
+   增加k == 1的特判
  */
 class Solution {
 	public static void main(String[] args) {
@@ -18,6 +19,9 @@ class Solution {
 
 	}
 	public int minimumDifference(int[] nums, int k) {
+		if (k == 1){
+			return 0;
+		}
 		Arrays.sort(nums);
 		int ans = Integer.MAX_VALUE;
 		for (int i = 0; i <= nums.length-k; i++){
